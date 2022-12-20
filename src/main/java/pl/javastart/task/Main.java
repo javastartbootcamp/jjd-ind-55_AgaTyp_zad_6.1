fc01ee98d00b7493a7a4c2a565483e959edfdb39
Agnieszka Typiak <agnieszka.typiak@mailbox.org>
2022-12-20 09:47:18 +0100

    Zmiana założeń

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -7,16 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner guess = new Scanner(System.in);
-
-        final int number = 120;
+        
         int userGuess;
 
         System.out.println("Podaj liczbę");
 
-        while ((userGuess = guess.nextInt()) != number) {
+        while (((userGuess = guess.nextInt()) % 3 != 0) || !(userGuess >= 100) || !(userGuess <= 200)) {
             if (userGuess % 3 != 0) {
                 System.out.println("Liczba nie jest podzielna przez 3");
-            } else if (userGuess < number) {
+            } else if (userGuess < 100) {
                 System.out.println("Podana liczba jest za mała");
             } else {
                 System.out.println("Podana liczba jest za duża");
